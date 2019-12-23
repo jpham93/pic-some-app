30dbcff5b5629eb67723e4d334721c3b43a10eeb
James Pham <jamespham93@yahoo.com>
2019-12-23 08:44:23 -0800

    AppContext contains state to persist photos

--- app/context/appContext.js
+++ app/context/appContext.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AppContext = React.createContext();
 
 const AppContextProvider = (props) => {
+  const [photos, setPhotos] = useState([]);
+
   return (
-    <AppContext.Provider value="">
+    <AppContext.Provider value={ photos }>
       { props.children }
     </AppContext.Provider>
   );
